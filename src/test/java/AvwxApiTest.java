2ff87d263999e49ab2a8c37bb95e8c66c6024f0c
siKruger <eddiwood2@gmail.com>
2021-08-04 23:05:33 +0200

    TAF tests

--- src/test/java/AvwxApiTest.java
+++ src/test/java/AvwxApiTest.java
@@ -1,5 +1,7 @@
 import api.AvwxRequests;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,11 +9,47 @@ import java.io.IOException;
 public class AvwxApiTest {
 
     @Test
-    public void getMetarTest() throws IOException {
+    public void getMetarIcaoTest() throws IOException {
         JSONObject res = AvwxRequests.getMetar("EDDF");
 
 
         JSONObject meta = (JSONObject) res.get("meta");
         meta.get("timestamp");
     }
+
+    @Test
+    public void getMetarIataTest() throws IOException {
+        JSONObject res = AvwxRequests.getMetar("FRA");
+
+
+        JSONObject meta = (JSONObject) res.get("meta");
+        meta.get("timestamp");
+    }
+
+    @Test
+    public void getTafIcaoTest() throws IOException {
+        JSONObject res = AvwxRequests.getTAF("EDDF");
+
+
+        JSONObject meta = (JSONObject) res.get("meta");
+        meta.get("timestamp");
+    }
+
+    @Test
+    public void getTafIataTest() throws IOException {
+        JSONObject res = AvwxRequests.getTAF("FRA");
+
+
+        JSONObject meta = (JSONObject) res.get("meta");
+        meta.get("timestamp");
+    }
+
+    @Test
+    public void getInvalidTafTest() throws IOException {
+        JSONObject res = AvwxRequests.getTAF("FRAS");
+
+
+        Assertions.assertThrows(JSONException.class, () -> res.get("meta"));
+    }
+
 }
