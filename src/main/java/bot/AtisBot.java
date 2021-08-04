962de676e6e245b1e56768cda49d0f27a369844f
sikruger <eddiwood2@gmail.com>
2021-08-04 22:38:34 +0200

    removed unused import

--- src/main/java/bot/AtisBot.java
+++ src/main/java/bot/AtisBot.java
@@ -1,20 +1,13 @@
 package bot;
 
-
 import com.github.manevolent.ts3j.command.CommandException;
-import com.github.manevolent.ts3j.identity.Identity;
 import com.github.manevolent.ts3j.identity.LocalIdentity;
 import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
 import io.github.cdimascio.dotenv.Dotenv;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.math.BigInteger;
-import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
-import java.security.spec.ECPoint;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
