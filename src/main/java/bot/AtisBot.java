3d7a575caec58149bf7d74e53389f9c2d590684d
siKruger <eddiwood2@gmail.com>
2021-08-05 22:34:19 +0200

    added sink to bot

--- src/main/java/bot/AtisBot.java
+++ src/main/java/bot/AtisBot.java
@@ -1,22 +1,21 @@
 package bot;
 
-import com.github.manevolent.ts3j.command.CommandException;
 import com.github.manevolent.ts3j.identity.LocalIdentity;
 import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
 import io.github.cdimascio.dotenv.Dotenv;
+import tts.OpusParameters;
+import tts.TeamspeakFastMixerSink;
 
 import java.io.File;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
+
+import static tts.TeamspeakFastMixerSink.AUDIO_FORMAT;
 
 public class AtisBot {
     LocalTeamspeakClientSocket client;
     Dotenv dotenv = Dotenv.load();
 
 
-    public AtisBot() throws CommandException, IOException, ExecutionException, InterruptedException, TimeoutException, GeneralSecurityException {
+    public AtisBot() throws Exception {
         client = new LocalTeamspeakClientSocket();
 
         LocalIdentity identity = LocalIdentity.read(new File("src\\main\\resources\\bot_identity.ini"));
@@ -26,6 +25,28 @@ public class AtisBot {
         client.setNickname("ATIS/METAR/TAF Bot");
         client.connect(dotenv.get("TEAMSPEAK_HOSTNAME"), dotenv.get("TEAMSPEAK_PASSWORD"), 20000);
         client.setDescription("Write !help to get a list of available commands.");
+
+        // Create a sink
+        TeamspeakFastMixerSink sink = new TeamspeakFastMixerSink(
+                AUDIO_FORMAT,
+                (int) AUDIO_FORMAT.getSampleRate() * AUDIO_FORMAT.getChannels() * 2/*4=32bit float*/,
+                new OpusParameters(
+                        20,
+                        96000, // 96kbps
+                        10, // max complexity
+                        0, // 0 expected packet loss
+                        false, // no VBR
+                        false, // no FEC
+                        true // OPUS MUSIC - channel doesn't have to be Opus Music ;)
+                )
+        );
+        client.setMicrophone(sink);
+        sink.start();
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new AtisBot();
     }
 
 
