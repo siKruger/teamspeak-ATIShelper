1daec318590622388f8853c628b506bdebdbe288
siKruger <eddiwood2@gmail.com>
2021-08-06 00:10:17 +0200

    NatoAlphabet ENUM

---
+++ src/main/java/converter/NatoAlphabet.java
@@ -0,0 +1,46 @@
+package converter;
+
+public enum NatoAlphabet {
+    A("Alpha"),
+    B("Bravo"),
+    C("Charlie"),
+    D("Delta"),
+    E("Echo"),
+    F("Foxtrot"),
+    G("Golf"),
+    H("Hotel"),
+    I("India"),
+    J("Juliet"),
+    K("Kilo"),
+    L("Lima"),
+    M("Mike"),
+    N("November"),
+    O("Oskar"),
+    P("Papa"),
+    Q("Quebec"),
+    R("Romeo"),
+    S("Sierra"),
+    T("Tango"),
+    U("Uniform"),
+    V("Viktor"),
+    W("Whiskey"),
+    X("Xray"),
+    Y("Yankee"),
+    Z("Zulu");
+
+
+    final String spoken;
+
+    NatoAlphabet(String spoken) {
+        this.spoken = spoken;
+    }
+
+    public static void main(String[] args) {
+        NatoAlphabet t = NatoAlphabet.valueOf("F");
+        System.out.println(t.spoken);
+    }
+
+    public String getSpoken() {
+        return this.spoken;
+    }
+}
