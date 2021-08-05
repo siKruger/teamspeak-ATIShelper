64b873d143a0ec007c6ccd96c2c739e832cc0ff9
Ben <ben.juri.paul@web.de>
2021-08-06 00:33:51 +0200

    can now devide between private and channel message replies with metar and taf appropriately

--- src/main/java/bot/AtisBot.java
+++ src/main/java/bot/AtisBot.java
@@ -1,5 +1,7 @@
 package bot;
 
+import api.AvwxRequests;
+import com.github.manevolent.ts3j.api.TextMessageTargetMode;
 import com.github.manevolent.ts3j.command.CommandException;
 
 import com.github.manevolent.ts3j.event.ClientPokeEvent;
@@ -10,6 +12,7 @@ import com.github.manevolent.ts3j.identity.Identity;
 import com.github.manevolent.ts3j.identity.LocalIdentity;
 import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,7 +34,7 @@ public class AtisBot implements TS3Listener {
     String liste[][] = new String[4][2];
 
 
-    public AtisBot() throws CommandException, IOException, ExecutionException, InterruptedException, TimeoutException, GeneralSecurityException {
+    public AtisBot() throws Exception {
         client = new LocalTeamspeakClientSocket();
 
         LocalIdentity identity = LocalIdentity.read(new File("src\\main\\resources\\bot_identity.ini"));
@@ -59,72 +62,66 @@ public class AtisBot implements TS3Listener {
     @Override
     public void onTextMessage(TextMessageEvent e) { // fetches message
         TS3Listener.super.onTextMessage(e);
-        System.out.println(e.getMessage());
         if (Pattern.matches("^!.+", e.getMessage())) { // checks if message is command
+            int r = 0;
+            if (e.getTargetMode().equals(TextMessageTargetMode.CLIENT)) {
+                r = e.getInvokerId();
+            }
             try {
-                messageLength(e.getMessage());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            } catch (CommandException ex) {
-                ex.printStackTrace();
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            } catch (TimeoutException ex) {
+                messageLength(e.getMessage(), r);
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
     }
 
-    public void messageLength(String msg) throws IOException, CommandException, InterruptedException, TimeoutException { // checks if message contains values
+    public void messageLength(String msg, int reciever) throws Exception { // checks if message contains values
         String[] part = msg.toUpperCase().substring(1).split("\s");
         String part1, part2;
 
         if (part.length == 2) {
             part1 = part[0];
             part2 = part[1];
-            System.out.println(part1 + "\n" + part2);
-            action(part1, part2);
+            action(part1, part2, reciever);
         } else if (part.length == 1) {
             part1 = part[0];
-            System.out.println(part1);
-            action(part1, "");
+            action(part1, "", reciever);
         }
     }
 
-    public void action(String part1, String part2) throws IOException, CommandException, InterruptedException, TimeoutException {
-        System.out.println("action!" + part1 + part2);
+    public void action(String part1, String part2, int reciever) throws Exception {
         if (!part2.equals("") && Pattern.matches("[A-Z0-9]{3,4}", part2) && !part2.equals("HELP")) {
             if (liste[0][0].equals(part1)) {
-                // getMetar(part2);
-                System.out.println("getMetar(part2);");
-                response("getMetar(part2);");
+                JSONObject getter = AvwxRequests.getMetar(part2);
+
+                response(String.valueOf(getter.get("sanitized")), reciever);
             } else if (liste[1][0].equals(part1)) {
-                // getAtis(part2);
-                System.out.println("getAtis(part2);");
-                response("getAtis(part2);");
+                //response("getAtis(part2);");
             } else if (liste[2][0].equals(part1)) {
-                // getTaf(part2);
-                System.out.println("getTaf(part2);");
-                response("getTaf(part2);");
+                JSONObject getter = AvwxRequests.getTAF(part2);
+
+                response(String.valueOf(getter.get("sanitized")), reciever);
             }
         } else {
             for (int x = 0; x <= liste.length; x++) {
                 if (part1.equals(liste[x][0])) {
-                    System.out.println(liste[x][1]);
-                    response(liste[x][1]);
+                    response(liste[x][1], reciever);
                     return;
                 }
             }
-            System.out.println(liste[3][1]);
-            response(liste[3][1]);
+            response(liste[3][1], reciever);
         }
     }
 
-    public void response(String msg) throws IOException, CommandException, InterruptedException, TimeoutException {
-        client.sendChannelMessage(client.getClientId(), msg);
+    public void response(String msg, int reciever) throws Exception {
+        if (reciever != 0) {
+            client.sendPrivateMessage(reciever, msg);
+        } else {
+            client.sendChannelMessage(client.getClientId(), msg);
+        }
     }
 
-    public static void main(String[] args) throws GeneralSecurityException, CommandException, IOException, ExecutionException, InterruptedException, TimeoutException {
+    public static void main(String[] args) throws Exception {
         new AtisBot();
     }
 
