7afdf81e4cbabf7cdf15d18858b261195a9eb278
siKruger <eddiwood2@gmail.com>
2021-08-07 01:01:54 +0200

    first iteration of refactoring

--- src/main/java/bot/AtisBot.java
+++ src/main/java/bot/AtisBot.java
@@ -7,6 +7,8 @@ import com.github.manevolent.ts3j.event.TS3Listener;
 import com.github.manevolent.ts3j.event.TextMessageEvent;
 import com.github.manevolent.ts3j.identity.LocalIdentity;
 import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
+import converter.Commands;
+import converter.FLC;
 import exception.AtisCooldownException;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.json.JSONObject;
@@ -61,7 +63,7 @@ public class AtisBot implements TS3Listener {
         client.addListener(this);
         sink.start();
 
-        Database.DBcreate(); // create the database for the commands - necessary
+        CommandList.DBcreate(); // create the database for the commands - necessary
     }
 
 
@@ -73,8 +75,10 @@ public class AtisBot implements TS3Listener {
             if (e.getTargetMode().equals(TextMessageTargetMode.CLIENT)) {
                 r = e.getInvokerId();
             }
+
+            //todo exception handling
             try {
-                messageLength(e.getMessage(), r);
+                commandAnalyzer(e.getMessage(), r);
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -85,115 +89,125 @@ public class AtisBot implements TS3Listener {
         String[] request = msg.toUpperCase().substring(1).split("\s");
         String cmmd = request[0], value1 = "", value2 = "", value3 = "";
 
-        if (part.length == 1) {
-            part1 = part[0];
-            action(part1, "", "", "", reciever);
-        } else if (part.length == 2) {
-            part1 = part[0];
-            part2 = part[1];
-            action(part1, part2, "", "", reciever);
-        } else if (part.length == 3) {
-            part1 = part[0];
-            part2 = part[1];
-            part3 = part[2];
-            action(part1, part2, part3, "",reciever);
-        } else if (part.length == 4) {
-            part1 = part[0];
-            part2 = part[1];
-            part3 = part[2];
-            part4 = part[3];
-            System.out.println(part1 +"\n"+  part2+ "\n"+part3+ "\n"+part4);
-            action(part1, part2, part3, part4, reciever);
+        switch (request.length) {
+            //command + 1 value (e.g. conversion, metar -> !atis eddh; !metar fra)
+            case 2 -> {
+                value1 = request[1];
+                action(cmmd, value1, "", "", reciever);
+            }
+
+            //command + 2 values (e.g conversion; !tas 32 42)
+            case 3 -> {
+                value1 = request[1];
+                value2 = request[2];
+            }
+
+            //command + 3 values (e.g conversion; !truealtitude 23 2323 3)
+            case 4 -> {
+                value1 = request[1];
+                value2 = request[2];
+                value3 = request[3];
+            }
         }
+
+        action(cmmd, value1, value2, value3, reciever);
     }
 
-    // this is going to be terrifying
-    public void action(String part1, String part2, String part3, String part4, int reciever) throws Exception {
-        if (!part2.equals("") && Pattern.matches("[A-Z0-9]{3,4}", part2) && !part2.equals("HELP") && part3.equals("")) {
-            if (Database.command[0][0].equals(part1)) {
-                JSONObject getter = AvwxRequests.getMetar(part2);
 
+    // this is going to be terrifying. YES IT IS
+    private void action(String command, String value1, String value2, String value3, int reciever) throws Exception {
+
+        //METAR/ATIS/TAF
+        if (!value1.equals("") && Pattern.matches("[A-Z0-9]{3,4}", value1) && !value1.equals("HELP") && value2.equals("")) {
+
+            Commands commandToExecute = Commands.valueOf(command);
+            if (commandToExecute.name().equals("METAR")) {
+                JSONObject getter = AvwxRequests.getMetar(value1);
                 response(String.valueOf(getter.get("sanitized")), reciever);
-            } else if (Database.command[1][0].equals(part1)) {
+                return;
+            }
 
+            if (commandToExecute.name().equals("ATIS")) {
                 try {
-                    Atis.generateAtis(part2, sink);
+                    Atis.generateAtis(value1, sink);
                 } catch (AtisCooldownException e) {
                     response("Bitte warte eine Minute...", reciever);
                 }
+                return;
+            }
 
-
-            } else if (Database.command[2][0].equals(part1)) {
-                JSONObject getter = AvwxRequests.getTAF(part2);
-
+            if (commandToExecute.name().equals("TAF")) {
+                JSONObject getter = AvwxRequests.getTAF(value1);
                 response(String.valueOf(getter.get("sanitized")), reciever);
             }
-        } else if (!part2.equals("") && Pattern.matches("\\d+", part2) && !part2.equals("HELP")) {
-            if (Database.command[4][0].equals(part1)) { // skip 3, because 3 is !HELP
-                response("" + FLC.kg2lb(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[5][0].equals(part1)) {
-                response("" + FLC.lb2kg(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[6][0].equals(part1)) {
-                response("" + FLC.kmh2kn(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[7][0].equals(part1)) {
-                response("" + FLC.kn2kmh(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[8][0].equals(part1)) {
-                response("" + FLC.km2nm(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[9][0].equals(part1)) {
-                response("" + FLC.nm2km(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[10][0].equals(part1)) {
-                response("" + FLC.m2ft(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[11][0].equals(part1)) {
-                response("" + FLC.ft2m(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[12][0].equals(part1)) {
-                response("" + FLC.c2f(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[13][0].equals(part1)) {
-                response("" + FLC.f2c(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[14][0].equals(part1)) {
-                response("" + FLC.hPa2inHg(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[15][0].equals(part1)) {
-                response("" + FLC.inHg2hPa(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[16][0].equals(part1)) {
-                response("" + FLC.h2min(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[17][0].equals(part1)) {
-                response("" + FLC.min2h(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[18][0].equals(part1)) {
-                response("" + FLC.ISA(Double.parseDouble(part2)), reciever);
-            } else if (Database.command[19][0].equals(part1)) { // can cause problems
-                response("" + FLC.endurance(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[20][0].equals(part1)) {
-                response("" + FLC.reqFuel(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[21][0].equals(part1)) {
-                response("" + FLC.ffl(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[22][0].equals(part1)) {
-                response("" + FLC.timeForDist(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[23][0].equals(part1)) {
-                response("" + FLC.distInTime(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[24][0].equals(part1)) {
-                response("" + FLC.speedByTimeForDist(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[25][0].equals(part1)) {
-                response("" + FLC.pressureAlt(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[26][0].equals(part1)) {
-                response("" + FLC.deltaTemp(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[27][0].equals(part1)) {
-                response("" + FLC.densityAlt((Double.parseDouble(part2)), Double.parseDouble(part3)), reciever);
-            } else if (Database.command[28][0].equals(part1)) {
-                response("" + FLC.trueAlt(Double.parseDouble(part2), Double.parseDouble(part3), Double.parseDouble(part4)), reciever);
-            } else if (Database.command[29][0].equals(part1)) {
-                response("" + FLC.TAS(Double.parseDouble(part2), Double.parseDouble(part3)), reciever);
+
+
+        } else if (!value1.equals("") && Pattern.matches("\\d+", value1) && !value1.equals("HELP")) {
+            if (CommandList.command[4][0].equals(command)) { // skip 3, because 3 is !HELP
+                response("" + FLC.kg2lb(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[5][0].equals(command)) {
+                response("" + FLC.lb2kg(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[6][0].equals(command)) {
+                response("" + FLC.kmh2kn(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[7][0].equals(command)) {
+                response("" + FLC.kn2kmh(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[8][0].equals(command)) {
+                response("" + FLC.km2nm(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[9][0].equals(command)) {
+                response("" + FLC.nm2km(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[10][0].equals(command)) {
+                response("" + FLC.m2ft(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[11][0].equals(command)) {
+                response("" + FLC.ft2m(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[12][0].equals(command)) {
+                response("" + FLC.c2f(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[13][0].equals(command)) {
+                response("" + FLC.f2c(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[14][0].equals(command)) {
+                response("" + FLC.hPa2inHg(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[15][0].equals(command)) {
+                response("" + FLC.inHg2hPa(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[16][0].equals(command)) {
+                response("" + FLC.h2min(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[17][0].equals(command)) {
+                response("" + FLC.min2h(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[18][0].equals(command)) {
+                response("" + FLC.ISA(Double.parseDouble(value1)), reciever);
+            } else if (CommandList.command[19][0].equals(command)) { // can cause problems
+                response("" + FLC.endurance(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[20][0].equals(command)) {
+                response("" + FLC.reqFuel(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[21][0].equals(command)) {
+                response("" + FLC.ffl(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[22][0].equals(command)) {
+                response("" + FLC.timeForDist(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[23][0].equals(command)) {
+                response("" + FLC.distInTime(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[24][0].equals(command)) {
+                response("" + FLC.speedByTimeForDist(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[25][0].equals(command)) {
+                response("" + FLC.pressureAlt(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[26][0].equals(command)) {
+                response("" + FLC.deltaTemp(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[27][0].equals(command)) {
+                response("" + FLC.densityAlt((Double.parseDouble(value1)), Double.parseDouble(value2)), reciever);
+            } else if (CommandList.command[28][0].equals(command)) {
+                response("" + FLC.trueAlt(Double.parseDouble(value1), Double.parseDouble(value2), Double.parseDouble(value3)), reciever);
+            } else if (CommandList.command[29][0].equals(command)) {
+                response("" + FLC.TAS(Double.parseDouble(value1), Double.parseDouble(value2)), reciever);
             } // this is horrifying and needs to be refactored ASAP
         } else {
-            for (int x = 0; x < Database.command.length; x++) {
-                if (part1.equals(Database.command[x][0])) {
-                    response(Database.command[x][1], reciever);
+            for (int x = 0; x < CommandList.command.length; x++) {
+                if (command.equals(CommandList.command[x][0])) {
+                    response(CommandList.command[x][1], reciever);
                     return;
                 }
             }
-            response(Database.command[3][1], reciever);
+            response(CommandList.command[3][1], reciever);
         }
     }
 
-    public void response(String msg, int reciever) throws Exception {
+    private void response(String msg, int reciever) throws Exception {
         if (reciever != 0) {
             client.sendPrivateMessage(reciever, msg);
         } else {
