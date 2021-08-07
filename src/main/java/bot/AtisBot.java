ce4e7a83603f35b50056215bf4e276289ee4ae7c
siKruger <eddiwood2@gmail.com>
2021-08-08 00:58:01 +0200

    adapted for new command structure

--- src/main/java/bot/AtisBot.java
+++ src/main/java/bot/AtisBot.java
@@ -1,17 +1,12 @@
 package bot;
 
-import api.AvwxRequests;
-import atis.Atis;
 import com.github.manevolent.ts3j.api.TextMessageTargetMode;
 import com.github.manevolent.ts3j.event.TS3Listener;
 import com.github.manevolent.ts3j.event.TextMessageEvent;
 import com.github.manevolent.ts3j.identity.LocalIdentity;
 import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
-import converter.Commands;
-import converter.FLC;
-import exception.AtisCooldownException;
+import commands.Commands;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.json.JSONObject;
 import tts.OpusParameters;
 import tts.TeamspeakFastMixerSink;
 
@@ -32,7 +27,7 @@ public class AtisBot implements TS3Listener {
 
     LocalTeamspeakClientSocket client;
     Dotenv dotenv = Dotenv.load();
-    TeamspeakFastMixerSink sink;
+    public static TeamspeakFastMixerSink sink;
 
     public AtisBot() throws Exception {
         client = new LocalTeamspeakClientSocket();
@@ -63,7 +58,6 @@ public class AtisBot implements TS3Listener {
         client.addListener(this);
         sink.start();
 
-        CommandList.DBcreate(); // create the database for the commands - necessary
     }
 
 
@@ -76,79 +70,24 @@ public class AtisBot implements TS3Listener {
                 r = e.getInvokerId();
             }
 
-            //todo exception handling
+            String[] request = e.getMessage().toUpperCase().substring(1).split("\s");
+            String command = request[0];
+
+            //Passenden ENUM finden
             try {
-                commandAnalyzer(e.getMessage(), r);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
+                Commands commandToExecute = Commands.valueOf(command);
+                //Executen der Action-Methode
+                this.response(commandToExecute.commandAction(request), r);
 
-    private void commandAnalyzer(String msg, int reciever) throws Exception { // checks if message contains values
-        String[] request = msg.toUpperCase().substring(1).split("\s");
-        String command = request[0], value1 = "", value2 = "", value3 = "";
-
-        switch (request.length) {
-            //command + 1 value (e.g. conversion, metar -> !atis eddh; !metar fra)
-            case 2 -> {
-                value1 = request[1];
-            }
-
-            //command + 2 values (e.g conversion; !tas 32 42)
-            case 3 -> {
-                value1 = request[1];
-                value2 = request[2];
-            }
-
-            //command + 3 values (e.g conversion; !truealtitude 23 2323 3)
-            case 4 -> {
-                value1 = request[1];
-                value2 = request[2];
-                value3 = request[3];
-            }
-        }
-
-        action(command, value1, value2, value3, reciever);
-    }
-
-
-    // this is going to be terrifying. YES IT IS
-    private void action(String command, String value1, String value2, String value3, int reciever) throws Exception {
-        Commands commandToExecute = Commands.valueOf(command);
-
-        //METAR/ATIS/TAF
-        if (!value1.equals("") && Pattern.matches("[A-Z0-9]{3,4}", value1) && !command.equals("HELP") && value2.equals("")) {
-
-            if (commandToExecute.name().equals("METAR")) {
-                JSONObject getter = AvwxRequests.getMetar(value1);
-                response(String.valueOf(getter.get("sanitized")), reciever);
-                return;
-            }
-
-            if (commandToExecute.name().equals("ATIS")) {
+            } catch (Exception unused) {
                 try {
-                    response("Playing ATIS for " + value1, reciever);
-                    Atis.generateAtis(value1, sink);
-                } catch (AtisCooldownException e) {
-                    response("Bitte warte eine Minute...", reciever);
+                    this.response("Type !help to get a list of available commands.", r);
+                } catch (Exception unused2) {
                 }
-                return;
             }
-
-            if (commandToExecute.name().equals("TAF")) {
-                JSONObject getter = AvwxRequests.getTAF(value1);
-                response(String.valueOf(getter.get("sanitized")), reciever);
-            }
-
-            // for every calculation
-        } else if (!value1.equals("") && Pattern.matches("\\d+", value1) && !command.equals("HELP")) {
-            response(FLC.calculations(command, value1, value2, value3), reciever);
-        } else {
-            // help
-            response(commandToExecute.getHelpText(commandToExecute.name()), reciever);
         }
     }
+
 
     private void response(String msg, int reciever) throws Exception {
         if (reciever != 0) {
